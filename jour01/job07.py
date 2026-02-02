4f805c9ca2bbb009a758e2557b61770ec236d1e0
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-02-02 11:34:16 +0100

    feat : complete exercice 7

---
+++ jour01/job07.py
@@ -0,0 +1,28 @@
+class Personnage() : 
+    def __init__(self, x, y) : 
+        self.x = x
+        self.y = y 
+
+    def gauche(self) : 
+        self.x -= 1 
+    
+    def droite(self) : 
+        self.x += 1 
+
+    def haut(self) : 
+        self.y += 1 
+    
+    def bas(self) : 
+        self.y -= 1 
+
+    def position(self) : 
+        print(f"La position du joueur est ({self.x}, {self.y})")
+
+joueur = Personnage(2,3)
+
+joueur.position()
+
+joueur.haut()
+joueur.droite()
+
+joueur.position()
