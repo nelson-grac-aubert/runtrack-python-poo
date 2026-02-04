a5d73d541249f3c10abce03ef364c48a522c3411
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-02-04 14:52:44 +0100

    feat : complete exercice 5

--- jour03/job05.py
+++ jour03/job05.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 # Créez un jeu de combat en utilisant la POO.
 
 # À tour de rôle, votre personnage et l’ennemi attaquent. Le but étant de
@@ -24,3 +27,86 @@
 # Implémenter le déroulement d’une partie en demandant au joueur le niveau
 # de difficulté et pensez à ajouter une méthode qui vérifie la santé de vos
 # personnages ainsi qu’une méthode permettant de vérifier qui a gagné.
+
+class Character():
+    def __init__(self, name : str, health : int) : 
+        self.__name = name
+        self.__health = health
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_health(self):
+        return self.__health
+    def set_health(self, new_health) : 
+        self.__health = new_health
+        
+    def attack(self, other):
+        damage = random.randint(10,15)
+        other.set_health(other.get_health() - damage)
+        print(f"{self.get_name()} has dealt {damage} damage to {other.get_name()}!!!")
+        print(f"{other.get_name()} has {other.get_health()} health remaining\n")
+    
+    def check_death(self):
+        if self.get_health() <= 0 : 
+            return True
+        else : 
+            return False
+
+class Game():
+    def __init__(self): 
+        self.__active_characters = []
+        self.__difficulty = self.chose_difficulty()
+        self.launch_game()
+        self.game_loop()
+
+    def chose_difficulty(self):
+        while True : 
+            choice = input("Chose a difficulty : easy, medium or hard : ")
+
+            if choice == "easy" or choice == "medium" or choice == "hard" : 
+                return choice
+            else : 
+                print("Difficulty must be one of the three, it's case sensitive, type more carefully")
+    def get_difficulty(self):
+        return self.__difficulty
+
+    def launch_game(self) : 
+        if self.get_difficulty() == "easy" : 
+            player_health = 100
+            enemy_health = 80
+
+        elif self.get_difficulty() == "medium" : 
+            player_health = 100
+            enemy_health = 90
+        else : 
+            player_health = 100
+            enemy_health = 100
+        
+        print(f"\nDifficulty {self.get_difficulty()} has been chosen")
+        print(f"Enemy spawned with {enemy_health} health")
+        print(f"Player spawned with {player_health} health\n")
+
+        player = Character("Player", player_health)
+        enemy = Character("Enemy", enemy_health)
+        self.__active_characters.append(player)
+        self.__active_characters.append(enemy)
+
+        time.sleep(2)
+        
+    def game_loop(self) : 
+        running = True
+        while running :
+            for element in self.__active_characters : 
+                target = self.__active_characters[0] if element == self.__active_characters[1] else self.__active_characters[1]
+                element.attack(target)
+                
+                check = target.check_death()
+                if check == True : 
+                    print(f"{element.get_name()} is victorious! {target.get_name()} is defeated!")
+                    running = False
+                    break
+
+                time.sleep(1)
+
+current_game = Game()
