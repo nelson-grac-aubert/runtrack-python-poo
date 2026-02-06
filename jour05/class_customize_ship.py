0d4aed351e86cd720e58d98a87c1d51397b694f2
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-02-06 13:20:57 +0100

    feat : improve game loop

--- jour05/class_customize_ship.py
+++ jour05/class_customize_ship.py
@@ -4,7 +4,11 @@ import time
 
 class CustomizeShip() : 
     def __init__(self) : 
+        self.__ship = Ship({})
         self.game_loop()
+
+    def get_ship(self) : 
+        return self.__ship
 
     def create_part(self) : 
         """ Return a new part made with the characteristics given by user inputs """
@@ -12,7 +16,7 @@ class CustomizeShip() :
 
         time.sleep(2)
         while True : 
-            name = input("\nWhat kind of part is it? type its name and press enter")
+            name = input("\nWhat kind of part is it? type its name and press enter ")
             if name == "" : 
                 print("\nEmpty entry, the part must have at least a character")
             else : 
@@ -20,30 +24,57 @@ class CustomizeShip() :
         
         time.sleep(2)
         while True : 
-            material = input(f"\nWhat is that {name} made of? type its material and press enter")
+            material = input(f"\nWhat is that {name} made of? type its material and press enter ")
             if material == "" : 
                 print("\nEmpty entry, the material must have at least a character")
             else : 
                 break
         
         time.sleep(1)
-        print(f"\n Ok, so we're making a {name} made of {material}")
+        print(f"\nOk, so we're making a {name} made of {material}, it's been added to your boat!")
         return Part(name, material)
 
-    def add_part_to_ship(self, new_part) : 
-        pass
+    def add_part_to_ship(self) : 
+        new_part = self.create_part()
+        self.get_ship().add_part(new_part)
         
-
     def start_your_ship(self): 
         print("\nLet's start your ship with just one part for now :)")
 
-        self.__ship = Ship({"1":self.create_part()})
+        self.add_part_to_ship()
+
+    def get_user_choice(self): 
+        print("\n ----- Here are your next options -----")
+        print("1 : add a part to your ship")
+        print("2 : replace a part with another")
+        print("3 : change a piece completely")
+        print("4 : display the current state of your ship")
+
+        while True : 
+            choice = input("Type 1, 2 or 3 and press enter ") 
+            if choice not in ["1", "2", "3","4"] : 
+                print("Input error : just type the number")
+            else : 
+                return choice
 
     def check_ship_state(self): 
         self.__ship.display_state()
+
 
     def game_loop(self): 
         
         self.start_your_ship()
         time.sleep(2)
         self.check_ship_state()
+        time.sleep(2)
+
+        while True : 
+            choice = self.get_user_choice()
+            if choice == "1" : 
+                self.add_part_to_ship()
+            elif choice == "4" : 
+                self.check_ship_state()
+
+            time.sleep(2)
+                
+
--- jour05/class_ship.py
+++ jour05/class_ship.py
@@ -1,3 +1,5 @@
+from class_part import Part
+
 class Ship:
     def __init__(self, parts : dict):
         self.__parts = parts
@@ -7,18 +9,21 @@ class Ship:
     def set_parts(self, new_parts):
         self.__parts = new_parts
 
+    def add_part(self, new_part : Part): 
+        self.get_parts()[str(len(self.get_parts())+1)] = new_part
+
     def display_state(self): 
         print("\n ----- Current state of the boat -----")
         for key in self.get_parts() : 
             print(self.get_parts()[key])
 
-    def replace_part(self, replaced_part_name, new_part):
+    def replace_part(self, replaced_part_name : str, new_part : Part):
         """ Replace a part with a brand new one """
         for key in self.get_parts(): 
             if self.get_parts()[key].get_name() == replaced_part_name : 
                 self.get_parts()[key] = new_part
     
-    def change_part(self, part_name, new_material): 
+    def change_part(self, part_name : str, new_material : str): 
         """ Pick a current ship piece by its name and change its material """
         for key in self.get_parts():
             if self.get_parts()[key].get_name() == part_name : 
